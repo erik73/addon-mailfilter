0f1b4e78e21a15803f9d2e788a665e29d4326bc7
Erik Hilton <erikhilton73@gmail.com>
2026-02-13 11:07:29 +0100

    Fix script paths

--- mailfilter/rootfs/etc/s6-s2-hook/disable-services.sh
+++ mailfilter/rootfs/etc/s6-s2-hook/disable-services.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2086,SC2016,SC2027
 if bashio::config.false "enable_antivirus"; then
     bashio::log.info "Disabling antivirus services"
-    rm -fr /etc/s6-overlay/s6-rc.d/user/freshclam
-    rm -fr /etc/s6-overlay/s6-rc.d/user/clamav
+    rm -fr /etc/s6-overlay/s6-rc.d/user/contents.d/freshclam
+    rm -fr /etc/s6-overlay/s6-rc.d/user/contents.d/clamav
     rm -rf /data/lib/clamav
 fi
